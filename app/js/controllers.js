98d8509e4d523642626ddafad45aad8e520de0c0
Jason Coon <jason.coon@firemon.com>
2014-05-29 17:09:21 -0500

    switched to angular and bootstrap

---
+++ app/js/controllers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  .controller('HomeCtrl', ['$scope', function ($scope) {
+
+  }])
+  .controller('ImagesCtrl', ['$scope', function ($scope) {
+
+  }])
+  .controller('DotsCtrl', ['$scope', function ($scope) {
+
+  }]);
